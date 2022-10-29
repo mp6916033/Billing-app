76ae4af465da40b81d08c3ffe26210a4c1fa4a4f
mp6916033 <89679777+mp6916033@users.noreply.github.com>
2022-10-29 12:52:53 +0530

    Create BillsState.ts

---
+++ src/stores/billsStores/state/BillsState.ts
@@ -0,0 +1,12 @@
+import { Bills } from "src/model/Bill";
+import { ReducerState } from "src/stores/StoreHelper";
+
+export class BillsState implements ReducerState {
+  public readonly loading: boolean | undefined;
+  public readonly success: boolean | undefined;
+  public readonly error: any | Error | undefined;
+
+  public readonly bills: Bills | undefined;
+  public readonly selectedBillId: number | undefined;
+  public readonly newBillId: number | undefined;
+}
