1a755eae5d12bfb9efe730df542e4b26aca5fe70
mp6916033 <89679777+mp6916033@users.noreply.github.com>
2022-10-29 12:28:39 +0530

    Create Routes.tsx

---
+++ src/Routes.tsx
@@ -0,0 +1,14 @@
+export const routes = {
+  home: {
+    view: () => "/home",
+  },
+  overview: {
+    view: () => "/",
+  },
+  add: {
+    view: () => "/form",
+  },
+  bills: {
+    view: () => "/bills",
+  },
+};
