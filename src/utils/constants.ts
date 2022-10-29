b2bf1df70c47384c8ccc4ec977b8e2c982a404fe
mp6916033 <89679777+mp6916033@users.noreply.github.com>
2022-10-29 12:34:35 +0530

    Create constants.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,213 @@
+export const categories = [
+  "Dominoes",
+  "Car Wash",
+  "Amazon",
+  "House Rent",
+  "tuition",
+  "Laundry",
+  "Vacation",
+];
+
+export const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+// const x = { 0: "January" };
+// type NumToMonth = typeof x;
+
+export const numToMonths: any = {
+  0: "January",
+  1: "February",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "September",
+  9: "October",
+  10: "November",
+  11: "December",
+};
+
+export const monthlyArrayObj = {
+  0: [],
+  1: [],
+  2: [],
+  3: [],
+  4: [],
+  5: [],
+  6: [],
+  7: [],
+  8: [],
+  9: [],
+  10: [],
+  11: [],
+};
+export const budget = {
+  bills: [
+    {
+      id: 1,
+      description: "Dominoes",
+      category: "FoodNDining",
+      amount: 430,
+      date: "01-02-2020",
+    },
+    {
+      id: 2,
+      description: "Car wash",
+      category: "utility",
+      amount: 500,
+      date: "01-06-2020",
+    },
+    {
+      id: 3,
+      description: "Amazon",
+      category: "shopping",
+      amount: 2030,
+      date: "01-07-2020",
+    },
+    {
+      id: 4,
+      description: "House rent",
+      category: "Food & Dining",
+      amount: 35900,
+      date: "01-03-2020",
+    },
+    {
+      id: 5,
+      description: "Tuition",
+      category: "education",
+      amount: 2200,
+      date: "01-12-2020",
+    },
+    {
+      id: 6,
+      description: "Laundry",
+      category: "Personal Care",
+      amount: 320,
+      date: "01-14-2020",
+    },
+    {
+      id: 7,
+      description: "Vacation",
+      category: "Travel",
+      amount: 3430,
+      date: "01-18-2020",
+    },
+  ],
+};
+
+export const bills = [
+  {
+    id: 1,
+    description: "Dominoes",
+    category: "FoodNDining",
+    amount: 430,
+    date: "01-02-2020",
+  },
+  {
+    id: 2,
+    description: "Car wash",
+    category: "utility",
+    amount: 500,
+    date: "01-06-2020",
+  },
+  {
+    id: 3,
+    description: "Amazon",
+    category: "shopping",
+    amount: 2030,
+    date: "01-07-2020",
+  },
+  {
+    id: 4,
+    description: "House rent",
+    category: "Food & Dining",
+    amount: 35900,
+    date: "01-03-2020",
+  },
+  {
+    id: 5,
+    description: "Tuition",
+    category: "education",
+    amount: 2200,
+    date: "01-12-2020",
+  },
+  {
+    id: 6,
+    description: "Laundry",
+    category: "Personal Care",
+    amount: 320,
+    date: "01-14-2020",
+  },
+  {
+    id: 7,
+    description: "Vacation",
+    category: "Travel",
+    amount: 3430,
+    date: "01-18-2020",
+  },
+];
+
+export const bills2 = [
+  {
+    id: 1,
+    description: "Dominoes",
+    category: "FoodNDining",
+    amount: 430,
+    date: "01-02-2020",
+  },
+  {
+    id: 2,
+    description: "Car wash",
+    category: "utility",
+    amount: 500,
+    date: "01-06-2020",
+  },
+  {
+    id: 3,
+    description: "Amazon",
+    category: "shopping",
+    amount: 2030,
+    date: "01-07-2020",
+  },
+  {
+    id: 4,
+    description: "House rent",
+    category: "Food & Dining",
+    amount: 35900,
+    date: "01-03-2020",
+  },
+  {
+    id: 5,
+    description: "Tuition",
+    category: "education",
+    amount: 2200,
+    date: "01-12-2020",
+  },
+  {
+    id: 6,
+    description: "Laundry",
+    category: "Personal Care",
+    amount: 320,
+    date: "01-14-2020",
+  },
+  {
+    id: 7,
+    description: "Vacation",
+    category: "Travel",
+    amount: 3430,
+    date: "01-18-2020",
+  },
+];
